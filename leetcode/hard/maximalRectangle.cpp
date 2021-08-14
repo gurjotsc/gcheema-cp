539388d6ae560a25c1459b3f972a504d9dc1785d
gurjotsc <gcheema@usc.edu>
2021-08-14 00:30:10 -0700

    maximalRectangle 2

--- leetcode/hard/maximalRectangle.cpp
+++ leetcode/hard/maximalRectangle.cpp
@@ -7,18 +7,19 @@ public:
         if(!m)
             return 0;
         int n = matrix[0].size();
-        dp.assign(m+1, vector<int> (n + 1));
+        dp.assign(2, vector<int> (n + 1));
         for(int i = 1; i <= m; ++i) {
+            int t = i % 2;
             for(int j = 1; j <= n; ++j) {
-                if(matrix[i - 1][j - 1] == '0') dp[i][j] = 0;
+                if(matrix[i - 1][j - 1] == '0') dp[t][j] = 0;
                 else {
-                    dp[i][j] = dp[i - 1][j] + 1;
+                    dp[t][j] = dp[!t][j] + 1;
                 }
 
-                int minHeight = dp[i][j];
+                int minHeight = dp[t][j];
                 for(int x = j; x >= 0; --x) {
                     int width = j - x + 1;
-                    minHeight = min(minHeight, dp[i][x]);
+                    minHeight = min(minHeight, dp[t][x]);
                     ans = max(ans, minHeight * width);
                 }
             }
