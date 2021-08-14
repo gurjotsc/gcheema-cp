f7a65c7bf7c0c1c1cbde9735946fe4d7e52dc8cd
gurjotsc <gcheema@usc.edu>
2021-08-14 00:23:26 -0700

    maximalSquare 2

--- leetcode/medium/maximalSquare.cpp
+++ leetcode/medium/maximalSquare.cpp
@@ -5,17 +5,18 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int ans = 0;
-        dp.assign(m + 1, vector<int> (n + 1));
+        dp.assign(2, vector<int> (n + 1));
         for(int i = 1; i <= m; ++i) {
+            int t = i % 2;
             for (int j = 1; j <= n; ++j) {
-                if(matrix[i-1][j-1] == '0') dp[i][j] = 0;
+                if(matrix[i-1][j-1] == '0') dp[t][j] = 0;
                 else {
-                    int top = dp[i-1][j];
-                    int diag = dp[i-1][j-1];
-                    int left = dp[i][j-1];
-                    dp[i][j] = min({top, diag, left}) + 1;
+                    int top = dp[!t][j];
+                    int diag = dp[!t][j-1];
+                    int left = dp[t][j-1];
+                    dp[t][j] = min({top, diag, left}) + 1;
                 }
-                ans = max(ans, dp[i][j]);
+                ans = max(ans, dp[t][j]);
             }
         }
         return ans*ans;
