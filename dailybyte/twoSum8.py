d974ca66e11b9836a91df24f32b6b9c3051f6bef
gurjotsc <gcheema@usc.edu>
2021-08-19 23:58:47 -0700

    twoSum8

---
+++ dailybyte/twoSum8.py
@@ -0,0 +1,10 @@
+def twoSum(arr, k):
+    for i in range(len(arr)):
+        if(i == len(arr) - 1): return False
+        try:
+            if(arr[i+1:].index(k - arr[i]) >= 0): return True
+        except:
+            continue;
+
+arr = [4, 2, 6, 5, 2]
+print(twoSum(arr, 4))
