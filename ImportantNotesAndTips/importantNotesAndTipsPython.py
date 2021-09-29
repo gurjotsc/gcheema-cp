3e6dadb2f37fdb94debd8d3877e0f70ea7e6d3f1
gurjotsc <gcheema@usc.edu>
2021-09-29 01:29:26 -0700

    notes

---
+++ ImportantNotesAndTips/importantNotesAndTipsPython.py
@@ -0,0 +1,8 @@
+#string slicing
+    #a = "Sammy Shark!"
+        a[beginning:middle:step]
+        a[5:10] = " Shar"
+        a[5:10:2] = " hr"
+        a[:-1] = "Sammy Shark"
+        a[::-1] = "!krahS ymmaS"
+        a[-4:-1] = "ark"
