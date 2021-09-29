c12072dceeaa54aa22b8f4d7cbe5ef71dba6254b
gurjotsc <gcheema@usc.edu>
2021-09-29 15:10:19 -0700

    dailyTemperatures 2

--- leetcode/medium/dailyTemperatures.cpp
+++ leetcode/medium/dailyTemperatures.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        unordered_map<int, int> m;
-        int max  = 0;
-        for(int i = temperatures.size() - 1; i >= 0; --i) {
-            if(i == temperatures.size() - 1) {
-                m[temperatures[i]] = i;
-                max = temperatures[i];
-                temperatures[i] = 0;
-                continue;
-            }
+        int N = temperatures.size();
+        vector<int> ans (N);
 
-            if(temperatures[i] >= max) {
-                m[temperatures[i]] = i;
-                max = temperatures[i];
-                temperatures[i] = 0;
-                continue;
+        //iterate down temperatures
+        for(int i = N - 1; i >= 0; --i){
+            int j = i+1; //j is the next index
+            //iterate from j through temperatures to find a value of temperatures[j] > temperatures[i]
+            //*Note this is considered O(N) since you are skipping through the array (I think O(N^2)) in worst case - see discussion
+            while(j < N && temperatures[j] <= temperatures[i]) {
+                if (ans[j] > 0) j = ans[j] + j; //If we know the next warmer day from ans[j] we know that no index until that day will satisfy this so we can move j to that index of the next warmer day to see if it is greater than day i
+                else j = temperatures.size();//If there is no warmer day after day j
             }
-
-            int tempIndex = INT_MAX;
-            for(int j = temperatures[i] + 1; j <= 100; ++j) {
-                if(m.find(j) != m.end() && m[j] < tempIndex) tempIndex = m[j];
-            }
-
-            m[temperatures[i]] = i;
-            temperatures[i] = tempIndex - i;
+            if(j < N) ans[i] = j - i; //If j is in temperatures we just set ans[i] = the index of the next warmer day minus the index i
         }
 
-        return temperatures;
+        return ans;
     }
 };
