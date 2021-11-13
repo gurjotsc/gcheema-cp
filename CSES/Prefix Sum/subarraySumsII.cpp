7675e1888ff6982258a6d6bfa6d86a944a8879e7
gurjotsc <gcheema@usc.edu>
2021-11-13 08:20:39 -0800

    checkWhetherTwoStringsAreAlmostEquivalent

--- CSES/Prefix Sum/subarraySumsII.cpp
+++ CSES/Prefix Sum/subarraySumsII.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
 using namespace std;
+using ll = long long;
 
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
 
-    long long n, x; cin >> n >> x;
-    vector<long long> prefix(n);
-    unordered_map<long long, long long> m;
+    ll n, x; cin >> n >> x;
+    vector<ll> prefix(n);
+    map<ll, ll> m;
     cin >> prefix[0];
     m[prefix[0]] = 1;
-    for(long long i = 1; i < n; ++i) {
+    for(ll i = 1; i < n; ++i) {
         cin >> prefix[i];
         prefix[i] += prefix[i-1];
     }
 
-    long long ans = 0;
-    for(long long i : prefix) {
+    ll ans = 0;
+    for(ll i : prefix) {
         ans += m[i-x];
         ++m[i];
     }
---
+++ leetcode/easy/checkWhetherTwoStringsAreAlmostEquivlent.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
+
+        //update frequnecies
+        for(auto i : word1) ++m1[i];
+        for(auto i : word2) ++m2[i];
+
+        //compare frequenceis
+        for(auto i : m1) if (m2[i.first] > i.second + 3 || m2[i.first] < i.second - 3) return false;
+        for(auto i : m2) if (m1[i.first] > i.second + 3 || m1[i.first] < i.second - 3) return false;
+
+        return true;
+    }
+};
