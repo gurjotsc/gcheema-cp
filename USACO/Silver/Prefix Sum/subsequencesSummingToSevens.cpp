04969f88a161854196ff05c0640ca428aeb8e383
gurjotsc <gcheema@usc.edu>
2021-11-13 00:23:13 -0800

    subsequencesSummingToSevens attempt

---
+++ USACO/Silver/Prefix Sum/subsequencesSummingToSevens.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+#define ll long long;
+
+int main() {
+    freopen("div7.in", "r", stdin);
+    freopen("div7.out", "w", stdout);
+
+    int n; cin >> n;
+    vector<int> prefix(n);
+    cin >> prefix[0];
+    unordered_map<int, int> m;
+
+    int ans = 0;
+    for(int i = 1; i < n; ++i) {
+        cin >> prefix[i];
+        prefix[i] += prefix[i-1];
+    }
+
+    for(int i = 0; i < n; ++i) {
+        prefix[i] %= 7;
+        if(m.find(prefix[i]) != m.end()) m[prefix[i]] = i;
+        else m[prefix[i]] = i;
+    }
+
+    for(int i = 0; i < n; ++i) {
+        ans = max(ans, m[prefix[i]] - i);
+    }
+    if(!ans) ++ans;
+
+    cout << ans;
+
+
+}
