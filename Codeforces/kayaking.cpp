405f514678d4e130217dd7e10d43e4b04c09a764
gurjotsc <gcheema@usc.edu>
2021-11-25 00:39:47 -0800

    kayaking

--- Codeforces/kayaking.cpp
+++ Codeforces/kayaking.cpp
@@ -2,42 +2,34 @@
 using namespace std;
 //using ll = long long;
 #define ll long long
+
 int main() {
     //freopen(".in", "r", stdin);
     //freopen(".out", "w", stdout);
 
     int n; cin >> n;
     vector<int> w(2*n);
-    int ans = 0;
+    int res = INT_MAX;
 
     for(int i = 0; i < 2*n; ++i) cin >> w[i];
     sort(w.begin(), w.end());
 
-    int maxDiff = 0;
-    for(int i = 0; i < (2*n) - 1; ++i) {
-        w[i] = abs(w[i] - w[i+1]);
-        maxDiff = max(maxDiff, w[i]);
+    //for loops for i and j assume that pair i and j (this overs all possibilites) are the two going in the individual kayak
+    for(int i = 0; i < 2*n; ++i) {
+        for(int j = i+1; j < 2*n; ++j) {
+            int currInstability = 0;
+            int prevDuo = 0; //stores the first person in the board (the person with the lower weight)
+
+            for(int k = 0; k < 2*n; ++k) { //iterates through the entire thing to calculate currInstability assuming i and j were in the single kayaks
+                if(k == i || k == j) continue; //if k is a person in an individual kayak
+                if(!prevDuo) prevDuo = w[k];
+                else {
+                    currInstability += abs(w[k] - prevDuo);
+                    prevDuo = 0;
+                }
+            }
+            res = min(res, currInstability);
+        }
     }
-    w[2*n - 1] = 0;
-    sort(w.begin(), w.end());
-
-    for(int i = 1; i < 2*n - 2; ++i) ans += w[i];
-
-    cout << ans;
-1 2 3 4
-1 1 1 0
-0 1 1 1
-
-
-3
-305 139 205 406 530 206
-139 205 206 305 406 530
-75  1   99  99  124 0
-0   1   75  99  99  124
-
-
-  166  66 201  124 324 0
-  0   66 124  166 201 324
-
-
+    cout << res;
 }
