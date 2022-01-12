8d503723ca75625c9ec408b146ed7114bec3d8c2
gurjotsc <gcheema@usc.edu>
2022-01-11 20:58:20 -0800

    iceSkating

--- Codeforces/iceSkating.cpp
+++ Codeforces/iceSkating.cpp
@@ -5,11 +5,13 @@ using ll = long long;
 int n;
 int numConnectedComponenets = 0;
 void dfs(vector<int>& xCoords, vector<int>& yCoords, int i) {
+    int tempX = xCoords[i];
+    int tempY = yCoords[i];
     xCoords[i] = 0;
     yCoords[i] = 0;
     for(int j = 0; j < n; ++j) {
         if(!xCoords[j]) continue;
-        if(xCoords[j] == xCoords[i] || yCoords[j] == yCoords[i])
+        if(xCoords[j] == tempX || yCoords[j] == tempY)
             dfs(xCoords, yCoords, j);
     }
 }
@@ -22,6 +24,7 @@ int main() {
         cin >> yCoords[i];
     }
     for(int i = 0; i < n; ++i) {
+        cout << endl;
         if(!xCoords[i]) continue;
         dfs(xCoords, yCoords, i);
         ++numConnectedComponenets;
