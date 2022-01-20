d8331e0cfa13365371bb792de7683b3aa9f07965
gurjotsc <gcheema@usc.edu>
2022-01-20 00:42:12 -0800

    badBoy

---
+++ Codeforces/badBoy.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+vector<pair<int, int>> dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};                 
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        ll n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+}
