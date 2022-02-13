b65405f3a645fdcad5bb304124f585607421ca53
gurjotsc <gcheema@usc.edu>
2022-02-13 01:13:34 -0800

    fashionabLee

---
+++ Codeforces/fashionabLee.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+                     
+int main() {
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        if(n%4) cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+}
