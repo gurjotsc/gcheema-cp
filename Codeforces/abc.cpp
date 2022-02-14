8f50717ddc4e98ff7c9c8d977f4ece334e17cd13
gurjotsc <gcheema@usc.edu>
2022-02-13 23:47:13 -0800

    abc

---
+++ Codeforces/abc.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+                     
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        string s; cin >> s;
+        int oCnt = 0, zCnt = 0;
+        if(n > 2 || s == "00" || s == "11") cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+}
