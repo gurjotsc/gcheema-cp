398db5d0705b8ce2c2cbca3d5a702e3024bcd4c4
gurjotsc <gcheema@usc.edu>
2022-03-22 22:10:58 -0700

    mexAndIncrements

---
+++ Codeforces/mexAndIncrements.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll inf = 1e18;
+
+template<typename T>
+std::istream& operator>>(std::istream& input, std::vector<T>& data) {
+    for (auto& item : data) {
+        input >> item;
+    }
+    return input;
+}
+
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    vector<ll> res(n+1, -1);
+    vector<int> freq(n+1);
+
+    for(auto& i : a) {
+        cin >> i;
+        ++freq[i];
+    }
+
+    sort(a.begin(), a.end());
+    
+    stack<int> s;
+    //stores the number of operations to get to the first mex
+    //The next mex that you need to solve for will require you getting the previous mex and then solving for how many operations will it take you to change the number ////in the stack to make the current one the mex (i.e take )
+    ll sum = 0ll; 
+    for(int i = 0; i <= n; ++i) {
+        if(i > 0 && !freq[i-1]) {
+            //you have no more numbers left to change then break
+            //all future numbers will require this number to be possible in the array, but clearly it isn't so all future numbers should output -1 as well
+            if(s.empty()) break; 
+
+            auto temp = s.top(); s.pop();
+            sum += i - temp - 1;
+        }
+
+        res[i] = sum + freq[i];
+
+        if(!i) continue;
+        while(freq[i-1] > 1) {
+            --freq[i-1];
+            s.push(i-1);
+        }
+    }
+
+    for(auto& i : res) cout << i << " ";
+    
+}
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int t; cin >> t;
+    while(t--) {
+        solve();
+        cout << "\n";
+    }
+}
