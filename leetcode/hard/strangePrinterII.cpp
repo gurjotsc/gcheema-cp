6d7be9d3923ca607070876e6abf90ab3dce6d51e
gurjotsc <gcheema@usc.edu>
2022-03-28 23:21:29 -0700

    strangePrinterII

--- leetcode/hard/strangePrinterII.cpp
+++ leetcode/hard/strangePrinterII.cpp
@@ -11,13 +11,15 @@ public:
         
         queue<int> q;
         unordered_set<int> seen;
-        for(int i = 1; i <= 60; ++i) 
+        for(int i = 0; i <= 60; ++i) 
             if(!inDeg[i]) q.push(i);
         
+        //basic topSort
         while(!q.empty()) {
             auto curr = q.front(); q.pop();
             seen.insert(curr);
             for(auto& child : graph[curr]) {
+                if(child == 2) cout << curr;
                 inDeg[child]--;
                 if(!inDeg[child]) q.push(child);
             }
@@ -29,24 +31,29 @@ public:
     
     void search(vector<vector<int>>& graph, const vector<vector<int>>& targetGrid, vector<int>& inDeg, int color) {
         int minX = INT_MAX, minY = INT_MAX, maxX = INT_MIN, maxY = INT_MIN;
+        //find bounds for outer region
         for(int i = 0; i < n; ++i)
             for(int j = 0; j < m; ++j) {
                 if(targetGrid[i][j] == color) {
                     minX = min(minX, i);
                     maxX = max(maxX, i);
-                    minY = min(minY, i);
-                    maxY = max(maxY, i);
+                    minY = min(minY, j);
+                    maxY = max(maxY, j);
                 }
             }
         
+        //color not found
         if(minX == INT_MAX) return;
         
+        //check if theres any different colors inside the bounds
         for(int i = minX; i <= maxX; ++i)
             for(int j = minY; j <= maxY; ++j) {
                 int interiorColor = targetGrid[i][j];
+                //if a color appers in the interior of another color (think example 3), then both the exterior and interior color will ++ thier inDegs.
+                //Thus they will never be added to the topSort q because there wouldn't be a source for those two nodes
                 if(interiorColor != color) {
-                    graph[color].push_back(interiorColor);
-                    ++inDeg[interiorColor];
+                    graph[interiorColor].push_back(color);
+                    inDeg[color]++;
                 }
             }
     }
