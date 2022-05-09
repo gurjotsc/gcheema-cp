8cca410bf75017782072466076467d62fce63178
gurjotsc <gcheema@usc.edu>
2022-05-08 23:01:37 -0700

    01matrix

---
+++ leetcode/medium/01Matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        m = mat.size(), n = mat[0].size();
+        vector<vector<int>> res(m, vector<int>(n, 1e9));
+        for(int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j) {
+                if(!mat[i][j]) {
+                    res[i][j] = 0;
+                    continue;
+                }
+                if(i > 0) res[i][j] = min(res[i][j], res[i-1][j] + 1);
+                if(j > 0) res[i][j] = min(res[i][j], res[i][j-1] + 1);
+            }
+        for(int i = m-1; i >= 0; --i)
+            for(int j = n-1; j >= 0; --j) {
+                if(!mat[i][j]) {
+                    res[i][j] = 0;
+                    continue;
+                }
+                if(i < m-1) res[i][j] = min(res[i][j], res[i+1][j] + 1);
+                if(j < n-1) res[i][j] = min(res[i][j], res[i][j+1] + 1);
+            }
+        return res;
+    }
+};
