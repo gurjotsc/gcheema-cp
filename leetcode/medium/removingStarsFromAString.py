ad2c12c1bad1e6706cfa35072651c5a5595503c4
gurjotsc <gcheema@usc.edu>
2022-09-13 22:59:03 -0700

    removingStarsFromAString

---
+++ leetcode/medium/removingStarsFromAString.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for i in s:
+            if i is not '*': stack.append(i)
+            else: stack.pop()
+        return ''.join(stack)
