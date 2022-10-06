3a0966a4580dd8f453df24ba3a3ab3b31368ed86
gurjotsc <gcheema@usc.edu>
2022-10-06 00:02:24 -0700

    countOfMatchesInTournament

---
+++ leetcode/easy/countOfMatchesInTournament.py
@@ -0,0 +1,5 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        if n == 1: return 0
+        if n % 2: return ((n-1)//2) + self.numberOfMatches(((n-1)//2)+1)
+        else: return (n//2) + self.numberOfMatches(n//2)
