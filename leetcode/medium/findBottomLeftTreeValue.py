43055d47926f01855f087f474a8f626469452bb2
gurjotsc <gcheema@usc.edu>
2022-10-10 01:00:37 -0700

    findBottomLeftTreeValue

---
+++ leetcode/medium/findBottomLeftTreeValue.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = [root]
+        for r in q:
+            q += filter(None, [r.right, r.left])
+        return r.val
