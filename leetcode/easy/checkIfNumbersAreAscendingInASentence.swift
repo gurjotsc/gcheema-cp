925becedc4d786cf67cdf0e50a08e60806c22ad1
gurjotsc <gcheema@usc.edu>
2023-01-21 23:50:06 -0800

    checkIfNumbersAreAscendingInASentence

---
+++ leetcode/easy/checkIfNumbersAreAscendingInASentence.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func areNumbersAscending(_ s: String) -> Bool {
+        let a = s.split(separator: " ").compactMap{Int($0)}
+        var prev = 0
+        for i in a {
+            guard i > prev else {return false}
+            prev = i
+        }
+        return true
+    }
+}
