439e1a31711c94da747e75e56ab1324b6a30b5dd
gurjotsc <gcheema@usc.edu>
2023-06-24 15:17:26 -0500

    bestBinaryString

---
+++ Codeforces/bestBinaryString.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll inf = 1e18;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+template<typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& data) {
+     for (auto& item : data) input >> item;
+     return input;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::vector<T>& data) {
+     for (const T& x : data) output << x << " " ;
+     return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::pair<T, T>& data) {
+    output << "{" << data.first << ", " << data.second << "}";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::unordered_map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::unordered_set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+ll modPow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+string solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    for (int i = n-1; i >= 0; --i) {
+        if(s[i] != '?') continue;
+        if(i == n-1) s[i] = '1';
+        else if (!i) s[i] = '0';
+        else{
+            if(s[i-1] != '?') s[i] = s[i-1];
+            else s[i] = s[i+1];
+        }
+    }
+    return s;
+}
+
+
+signed main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int t; cin >> t;
+    while (t--) cout << solve() << "\n";
+}
