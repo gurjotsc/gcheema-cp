89eeef60d25eea52f56a60e759f31a6404c2135f
gurjotsc <gcheema@usc.edu>
2023-07-05 00:02:58 -0500

    xxxxx

---
+++ Codeforces/xxxxx.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll inf = 1e18;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+template<typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& data) {
+     for (auto& item : data) input >> item;
+     return input;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::vector<T>& data) {
+     for (const T& x : data) output << x << " " ;
+     return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::pair<T, T>& data) {
+    output << "{" << data.first << ", " << data.second << "}";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::unordered_map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::unordered_set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+ll modPow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+ll solve() {
+    ll n, x; cin >> n >> x;
+	vector<ll> a(n); cin >> a;
+    ll l = -1, r = 0;
+	ll sm = 0ll;
+	//the first and last elements that aren't divisible by x will create the longest
+	//subarrays where the sum isn't divisble by x. For instance, removing a number
+	//not divisible by x from a sum that IS divisible x, will then make the sum not
+	//divisible by x.
+	for(int i = 0; i < n; ++i) {
+		if(a[i]%x){
+			l = (l == -1) ? i : l; //idx of first element not divisible by x
+			r = i; //will eventually sore last element not divisible by x
+		}
+		sm += a[i];
+	}
+	if(sm%x) return n; //whole array sum isn't divisible by x
+	if(l == -1) return -1; //there is no element that isn't divisble by x
+	
+	//remove either the prefix or suffix that
+	// will make the sum not divisible by x
+	return n-(min(l+1, n-r));
+
+}
+
+
+signed main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int t; cin >> t;
+    while (t--) cout << solve() << "\n";
+}
