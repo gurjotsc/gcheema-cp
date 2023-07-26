849b111b8c23289842629ccf98c710dc997c5aad
gurjotsc <gcheema@usc.edu>
2023-07-26 00:11:49 -0500

    doubleEndedStrings

---
+++ Codeforces/doubleEndedStrings.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const ll inf = 1e18;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+template<typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& data) {
+     for (auto& item : data) input >> item;
+     return input;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::vector<T>& data) {
+     for (const T& x : data) output << x << " " ;
+     return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::pair<T, T>& data) {
+    output << "{" << data.first << ", " << data.second << "}";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::unordered_map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::unordered_set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+ll modPow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll solve() {
+    string a, b; cin >> a >> b;
+    ll n = a.size(), m = b.size();
+    ll res = 0ll;
+    for(ll sz = 1; sz <= min(n, m); ++sz)
+        for(ll i = 0ll; i + sz <= n; ++i)
+            for(ll j = 0ll; j + sz <= m; ++j) {
+                if(a.substr(i, sz) == b.substr(j, sz))
+                    res = max(res, sz);
+            }
+    return n+m-(2*res);
+}
+
+
+signed main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int t; cin >> t;
+    while (t--) cout << solve() << "\n";
+}
