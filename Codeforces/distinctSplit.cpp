471151a42b5514156cdaa71d742c28e0eae4e513
gurjotsc <gcheema@usc.edu>
2023-07-30 01:45:00 -0500

    distinctSplit

---
+++ Codeforces/distinctSplit.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const ll inf = 1e18;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+template<typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& data) {
+     for (auto& item : data) input >> item;
+     return input;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::vector<T>& data) {
+     for (const T& x : data) output << x << " " ;
+     return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::pair<T, T>& data) {
+    output << "{" << data.first << ", " << data.second << "}";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::unordered_map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::unordered_set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+ll modPow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+ll solve() {
+    ll n; cin >> n;
+    string s; cin >> s;
+    vector<ll> pre(n), suf(n);
+    set<char> st;
+    ll res = 0ll;
+    for(int i = 0; i < n; ++i) {
+        st.insert(s[i]);
+        pre[i] = st.size();
+    }
+    st.clear();
+    for(int i = n-1; i >= 0; --i) {
+        st.insert(s[i]);
+        suf[i] = st.size();
+    }
+
+    for(int i = 1; i < n; ++i)
+        res = max(res, pre[i-1]+suf[i]);
+    
+    return res;
+}
+
+
+signed main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int t; cin >> t;
+    while (t--) cout << solve() << "\n";
+}
