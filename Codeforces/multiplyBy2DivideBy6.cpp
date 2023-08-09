867f3940300a317a956c3eb82dbac25bfed9d3f2
gurjotsc <gcheema@usc.edu>
2023-08-08 21:54:14 -0500

    multiplyBy2DivideBy6

---
+++ Codeforces/multiplyBy2DivideBy6.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using schlong = long;
+using ull = unsigned long long;
+const ll inf = 1e18;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+template<typename T>
+std::istream& operator >>(std::istream& input, std::vector<T>& data) {
+     for (auto& item : data) input >> item;
+     return input;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::vector<T>& data) {
+     for (const T& x : data) output << x << " " ;
+     return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::pair<T, T>& data) {
+    output << "{" << data.first << ", " << data.second << "}";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T, typename U>
+std::ostream& operator <<(std::ostream& output, const std::unordered_map<T, U>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << "{" << it->first << ", " << it->second << "} ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+template <typename T>
+std::ostream& operator <<(std::ostream& output, const std::unordered_set<T>& data) {
+    for(auto it = data.begin(); it != data.end(); ++it) output << *it << " ";
+    return output;
+}
+ll modPow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+schlong solve() {
+    schlong n; cin >> n;
+    schlong cnt2 = 0, cnt3 = 0;
+    while(!(n%2)) {
+        n /= 2;
+        ++cnt2;
+    }
+    while(!(n%3)) {
+        n /= 3;
+        ++cnt3;
+    }
+
+    if(cnt2 > cnt3) return -1;
+    if(n != 1) return -1;
+    return (cnt3-cnt2)+(cnt3);
+}
+
+
+signed main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int t; cin >> t;
+    while (t--) cout << solve() << "\n";
+}
